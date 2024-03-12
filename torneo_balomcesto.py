672cda2f053b45151cf857952a4eb7e3cfcd0f29
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-03-12 13:57:15 -0500

    Add files via upload

---
+++ torneo_balomcesto.py
@@ -0,0 +1,27 @@
+import os
+os.system('cls')
+
+var_equipo1 = input('Ingrese el nombre del equipo 1 -> ')
+puntos1Int = 0
+var_equipo2 = input('Ingrese el nombre del equipo 2 -> ')
+puntos2Int = 0
+var_equipo3 = input('Ingrese el nombre del equipo 3 ->  ')
+puntos3Int = 0
+var_equipo4 = input('Ingrese el nombre del equipo 4 -> ')
+puntos4Int = 0
+
+# valor por lanzamiento
+
+lanz1Int = 1
+lanz2Int = 2
+lan3Int = 3
+controlBln =  True
+
+while controlBln == True:
+    os. system 
+    print('---MENÚ DE EQUIPOS---')
+    print('1. ',var_equipo1,'Puntos: ',puntos1Int,'\n2. ',var_equipo2,'Puntos: ',puntos2Int,'\n3. ',var_equipo3,'Puntos: ',puntos3Int,'\n4. ',var_equipo4,'Puntos: ',puntos4Int,'\n5. Finalizar')
+    op_equipoInt =  int(input('\nSeleccione una opción -> '))
+    op_lanzamientoStr = input('---OPCIÓN DE LANZAMIENTOS---\n\n1. Lanzamiento\n2. Lanzamiento\n3. Lnzamiento\n -> ')
+
+    
